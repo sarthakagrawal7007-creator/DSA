e5761351e7b4cce2538d5ff242575f8908bccb1b
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-02-02 16:07:39 +0530

    Time: 0 ms (100%), Space: 43.9 MB (55.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,9 +7,13 @@ class Solution {
         // Compare from the back and place larger element
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+                nums1[k] = nums1[i];
+                k--;
+                i--;
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
     
         }
