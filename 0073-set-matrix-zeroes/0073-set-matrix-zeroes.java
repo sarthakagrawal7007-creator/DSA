4e129772214a2d615057f6ab9d87c6582c95433d
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-07-30 07:58:14 +0530

    Time: 1 ms (97.61%), Space: 47.8 MB (25.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,24 +2,49 @@ class Solution {
     public void setZeroes(int[][] num) {
       int n=num.length;
       int m=num[0].length;
-
-      boolean row[]=new boolean[n];
-      boolean column[]=new boolean[m];
-        for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <m ; j++) {
-                if (num[i][j]==0){
-                    row[i]=true;
-                    column[j]=true;
-            }
+       
+     //check 1st row is impacted or not 
+     boolean firstRow=false;
+     for(int i=0;i<m;i++){
+        if(num[0][i]==0){
+            firstRow=true;
         }
-      }for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <m ; j++) {
-                if (row[i]){
-                    num[i][j]=0;
-                }else if (column[j]){
-                    num[i][j]=0;
-                }
+     }
+     //check 1st column is impacted or not 
+     boolean firstCol=false;
+     for(int j=0;j<n;j++){
+        if(num[j][0]==0){
+            firstCol=true;
+     }
+    }
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<m;j++){
+            if(num[i][j]==0){
+                num[i][0]=0;
+                num[0][j]=0;
             }
         }
     }
+
+    for(int i=1;i<n;i++){
+     for(int j=1;j<m;j++){
+     if(num[i][0]==0||num[0][j]==0){
+          num[i][j]=0;
+         }
+       }
+    }
+
+    if(firstRow){
+        for(int j=0;j<m;j++){
+            num[0][j]=0;
+        }
+    }
+
+    if(firstCol){
+        for(int i=0;i<n;i++){
+            num[i][0]=0;
+        }
+    }
+}
 }
