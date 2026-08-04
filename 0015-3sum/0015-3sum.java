3f8a2e294c78402678f43f539c87875ef54e258f
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-04 13:10:17 +0530

    Time: 28 ms (97.07%), Space: 60.4 MB (7.7%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,39 @@
 class Solution {
-    //SARTHAK AGRAWAL
-        public static List<List<Integer>> threeSum(int[] nums) {
-     List<List<Integer>> result=new ArrayList<>();
-     int n=nums.length;
-     if (n<3){
-         return result;
-     }//sort
-        Arrays.sort(nums);
-     //fixing n1 element
-        for (int i = 0; i <n-2 ; i++) {
-            if (i>0 && nums[i]==nums[i-1]){
-               continue;
+    public List<List<Integer>> threeSum(int[] num) {
+         List<List<Integer>>l1=new ArrayList<>();
+       // int num[]={-1,0,1,2,-1,-4,0,2};
+        Arrays.sort(num);
+        int n=num.length;
+         if (num.length < 3){
+            return l1;}
+        for (int i = 0; i <=n-3 ; i++) {
+            if (i>0 && num[i]==num[i-1]){
+                continue;
             }
-            int n1=nums[i];
+            int n1=num[i];
             int target=-n1;
-            List<List<Integer>> pairs=twoSum(nums,target,i+1,n-1);
-            result.addAll(pairs);
+            Twosum(target,i+1,n-1,num,l1);
         }
-     return result;
+        return l1;
     }
-    public static  List<List<Integer>>twoSum(int[] nums,int target,int i,int j){
-        List<List<Integer>> pairs  =new ArrayList<>();
-      while (i<j){
-           if(nums[i]+nums[j]>target) {
-             j--;
-          }else if (nums[i]+nums[j]<target) {
-             i++;
-          }
-          else {
-             while (i<j && nums[i]==nums[i+1]) i++;
-             while (i<j && nums[j]==nums[j-1]) j--;
-               pairs.add(Arrays.asList(-target, nums[i], nums[j]));
-               i++;
-               j--;
-          }
-      }
-      return pairs;
+    public static void Twosum(int target, int i, int j, int num[], List<List<Integer>> l1){
+        while (i<j){
+            if ((num[i]+num[j])>target){
+                j--;
+            }else if((num[i]+num[j])<target){
+                i++;
+            }else {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(-target);
+                temp.add(num[i]);
+                temp.add(num[j]);
+                l1.add(temp);
+                while (i < j&&num[i]==num[i+1]){i++;}
+                while (i < j&&num[j]==num[j-1]){j--;}
+                i++;
+                j--;
+            }
+           // l1.add(temp);
+        }
     }
 }
