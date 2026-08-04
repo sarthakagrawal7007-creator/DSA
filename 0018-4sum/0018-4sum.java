795dd4eaa903de47bcd69957c3164886bd9f7d12
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-04 17:28:54 +0530

    Time: 18 ms (90.48%), Space: 46 MB (21.84%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,51 +1,43 @@
 class Solution {
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-          List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-
-        if (n < 4) return result;
-
-        Arrays.sort(nums);
-
-        for (int i = 0; i < n - 3; i++) {
-
-            // Skip duplicate for first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < n - 2; j++) {
-
-                // Skip duplicate for second number
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right) {
-
-                    // Use long to avoid integer overflow
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        left++;
-                        right--;
-
-                        // Skip duplicates for third number
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-
-                        // Skip duplicates for fourth number
-                        while (left < right && nums[right] == nums[right + 1]) right--;
-                    }
-                    else if (sum < target) {
-                        left++;
-                    }
-                    else {
-                        right--;
-                    }
+    public static List<List<Integer>> fourSum(int[] num, int target) {
+       
+       
+         List<List<Integer>>l1=new ArrayList<>();
+        //int num[]={1,0,-1,0,-2,2};
+         Arrays.sort(num);
+         int n=num.length;
+        for (int i = 0; i <n-2 ; i++) {
+            if (i>0&&num[i]==num[i-1]){
+                continue;
+            }
+            for (int a = i+1; a <num.length-1 ; a++) {
+                if (a > i + 1 &&num[a]==num[a-1]){
+                    continue;
+                }
+            int j=a+1;
+            int k=n-1;
+            while (j<k){
+                long sum =(long) num[i] + num[a] + num[j] + num[k];
+                if (sum>target){
+                    k--;
+                }else if (sum<target) {
+                    j++;
+                }else if(sum==target) {
+                   List<Integer> temp=new ArrayList<>();
+                   temp.add(num[i]);
+                   temp.add(num[a]);
+                    temp.add(num[j]);
+                    temp.add(num[k]);
+                    l1.add(temp);
+                    while (j < k && num[j] == num[j + 1]){j++;}
+                    while (j < k && num[k] == num[k - 1]){k--;}
+                    j++;
+                    k--;
                 }
             }
-        }
-        return result;
+        }}
+
+        return l1;
     }
-}
+    }
+      
