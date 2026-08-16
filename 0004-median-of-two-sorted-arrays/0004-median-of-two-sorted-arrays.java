6da9bbb3bf6f5a4ca65c54af3fb7ed7177c3e8ef
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-16 21:32:23 +0530

    Time: 2 ms (47.14%), Space: 48.8 MB (72%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,58 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-         int temp[]=new int[nums1.length+nums2.length];
-       int i=0;
+         int s=nums1.length+nums2.length;
+       int idx1=s/2-1;
+       int idx2=s/2 ;
+        int value1 = 0;
+        int value2 = 0;
+
+        int i=0;
        int j=0;
        int k=0;
        while (i<nums1.length && j<nums2.length ){
            if (nums1[i]>nums2[j]){
-               temp[k]=nums2[j];
+               if (k==idx1){
+                   value1=nums2[j];
+               }
+               if (k==idx2){
+                   value2=nums2[j];
+               }
                j++;
            }else {
-               temp[k]=nums1[i];
+               if (k==idx1){
+                   value1=nums1[i];
+               }
+               if (k==idx2){
+                   value2=nums1[i];
+               }
                i++;
            }
            k++;
        }
        while (i<nums1.length){
-           temp[k]=nums1[i];
+           if (k==idx1){
+               value1=nums1[i];
+           }if (k==idx2){
+               value2=nums1[i];
+           }
            i++;
            k++;
        }
         while (j<nums2.length){
-            temp[k]=nums2[j];
+            if (k==idx1){
+                value1=nums2[j];
+            }
+            if (k==idx2){
+                value2=nums2[j];
+            }
             j++;
             k++;
         }
         double median=-1;
-       if (temp.length%2==0){
-           median=(temp[temp.length/2]+temp[temp.length/2-1])/2.0;
-
+       if (s%2==0){
+           median=(value1+value2)/2.0;
        }else {
-           median=temp[temp.length/2];
+           median=value2;
        }
        return median;
     }
