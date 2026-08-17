36cfbec6ca7448571c58da11ed2543d46f6b591c
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-17 13:10:30 +0530

    Time: 1 ms (100%), Space: 48.7 MB (72.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,59 +1,68 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-         int s=nums1.length+nums2.length;
-       int idx1=s/2-1;
-       int idx2=s/2 ;
-        int value1 = 0;
-        int value2 = 0;
-
-        int i=0;
-       int j=0;
-       int k=0;
-       while (i<nums1.length && j<nums2.length ){
-           if (nums1[i]>nums2[j]){
-               if (k==idx1){
-                   value1=nums2[j];
-               }
-               if (k==idx2){
-                   value2=nums2[j];
-               }
-               j++;
-           }else {
-               if (k==idx1){
-                   value1=nums1[i];
-               }
-               if (k==idx2){
-                   value2=nums1[i];
-               }
-               i++;
-           }
-           k++;
-       }
-       while (i<nums1.length){
-           if (k==idx1){
-               value1=nums1[i];
-           }if (k==idx2){
-               value2=nums1[i];
-           }
-           i++;
-           k++;
-       }
-        while (j<nums2.length){
-            if (k==idx1){
-                value1=nums2[j];
-            }
-            if (k==idx2){
-                value2=nums2[j];
-            }
-            j++;
-            k++;
+        // Always apply binary search on the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        double median=-1;
-       if (s%2==0){
-           median=(value1+value2)/2.0;
-       }else {
-           median=value2;
-       }
-       return median;
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+
+            // Partition of nums1
+            int cut1 = low + (high - low) / 2;
+
+            // Partition of nums2
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            // Left and right values of nums1
+            int left1 = (cut1 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums1[cut1 - 1];
+
+            int right1 = (cut1 == m)
+                    ? Integer.MAX_VALUE
+                    : nums1[cut1];
+
+            // Left and right values of nums2
+            int left2 = (cut2 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums2[cut2 - 1];
+
+            int right2 = (cut2 == n)
+                    ? Integer.MAX_VALUE
+                    : nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Total length is even
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(left1, left2)
+                            + Math.min(right1, right2)) / 2.0;
+                }
+
+                // Total length is odd
+                else {
+                    return Math.max(left1, left2);
+                }
+            }
+
+            // We took too many elements from nums1
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+
+            // We need more elements from nums1
+            else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0;
     }
 }
