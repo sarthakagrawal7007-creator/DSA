9ece97df36dd02ec37f84575c62efea86c5af206
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-18 16:57:36 +0530

    Time: 5 ms (68.81%), Space: 46.4 MB (9.43%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -9,7 +9,7 @@ class Solution {
                 x+=1;
             }else if (ch=='U'){
                 y+=1;
-            }else if (ch=='D'){
+            }else {
                 y-=1;
             }
         }
