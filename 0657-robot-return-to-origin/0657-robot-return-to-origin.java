25514861a38b4c8bc12b03775dc1525f35d5be62
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-18 16:57:19 +0530

    Time: 5 ms (68.81%), Space: 46.3 MB (17.96%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String str) {
+         int x=0;
+       int y=0;
+        for (char ch:str.toCharArray()) {
+            if (ch=='L'){
+                x-=1;
+            }else if (ch=='R'){
+                x+=1;
+            }else if (ch=='U'){
+                y+=1;
+            }else if (ch=='D'){
+                y-=1;
+            }
+        }
+        if (x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+}
