a6963be99945e56c6c2e39c5fc0338b6fc5ee3c0
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-18 10:44:39 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.88%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -6,17 +6,32 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
         int i=0;
-        int j=m*n-1;
+        int j=n-1;
         while (i<=j ){
             int mid=i+(j-i)/2;
-             if (matrix[mid/m][mid%m]==target){
-                 return true;
+             if (matrix[mid][0]<= target && target<=matrix[mid][m-1]){
+                 return Rows(matrix,target,mid);
              }
-             if (matrix[mid/m][mid%m]>target){
-                j=mid-1;
+             if (matrix[mid][m-1]<target){
+                i=mid+1;
              }else {
-                 i=mid+1;
+                 j=mid-1;
              }
+        }
+        return false;
+    }
+    public static boolean Rows(int matrix[][],int target,int l){
+        int i=0;
+        int j=matrix[0].length-1;
+        while (i<=j){
+            int mid=i+(j-i)/2;
+            if (matrix[l][mid]==target){
+                return true;
+            }else if (matrix[l][mid]<target){
+                i=mid+1;
+            }else {
+                j=mid-1;
+            }
         }
         return false;
     }
