f24780fde1ce40f83f07b88c334247e10ad17440
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-18 17:02:55 +0530

    Time: 4 ms (98.63%), Space: 46.2 MB (26.99%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -4,11 +4,10 @@ class Solution {
        int y=0;
         for (char ch:str.toCharArray()) {
             switch (ch) {
-                case 'L' -> x--;
-                case 'R' -> x++;
-                case 'U' -> y++;
-                case 'D' -> y--;
-    
+                case 'L' : x--;break;
+                case 'R' : x++;break;
+                case 'U' : y++;break;
+                case 'D' : y--;break;
         }
         }
     return (x==0 && y==0);
