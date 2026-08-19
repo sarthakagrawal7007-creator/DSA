d39544bb2957470fa295111073d62478d11127a9
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-19 14:44:42 +0530

    Time: 1 ms (99.94%), Space: 45.3 MB (66.55%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compress(char[] ch) {
+         int m=0;
+     for (int i = 0; i <ch.length ; i++) {
+            int count =1;
+            while (i<ch.length-1 && ch[i]==ch[i+1]){
+                count++;
+                i++;
+            }
+            ch[m]=ch[i];
+            m++;
+            if (count > 1) {
+                m = rec(count, ch, m);
+            }
+        }
+        return m;
+    }
+   public static int rec(int count, char[] ch, int m) {
+                    if (count == 0) {
+                        return m;
+                    }
+
+                    int r = count % 10;
+                    count = count / 10;
+
+                    m = rec(count, ch, m);
+
+                    ch[m] = (char)(r + '0');
+                    m++;
+
+                    return m;
+                }
+
+}
