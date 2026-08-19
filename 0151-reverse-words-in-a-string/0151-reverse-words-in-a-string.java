c73780ba61aa34f70ddef65f261639f206160141
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-19 16:44:21 +0530

    Time: 6 ms (80.67%), Space: 46.6 MB (18.3%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,11 @@
+import java.util.StringTokenizer;
+class Solution {
+    public String reverseWords(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        String str1="";
+        while (st.hasMoreTokens()){
+            str1=st.nextToken()+" "+str1;
+        }
+        return str1.trim();
+    }
+}
