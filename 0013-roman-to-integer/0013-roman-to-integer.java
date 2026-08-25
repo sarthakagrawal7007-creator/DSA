e361c87d6d7333bbc527bcf7c2996a398fb404bd
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-25 22:25:31 +0530

    Time: 2 ms (100%), Space: 46.4 MB (62.55%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,30 +1,32 @@
 class Solution {
-    public int romanToInt(String str) {
-         HashMap<Character,Integer> mp=new HashMap<>();
-        mp.put('I',1);
-        mp.put('V',5);
-        mp.put('X',10);
-        mp.put('L',50);
-        mp.put('C',100);
-        mp.put('D',500);
-        mp.put('M',1000);
-        //int sum=0;
-        int j=0;
-        for (int i = 0; i <str.length() ; i++) {
-            if ((i<str.length()-1&&str.charAt(i)=='I')&&(str.charAt(i+1)=='V'||str.charAt(i+1)=='X')){
-                  j+=mp.get(str.charAt(i+1))-mp.get(str.charAt(i));
-                  i++;
-            }else if((i<str.length()-1&&str.charAt(i)=='X')&&(str.charAt(i+1)=='L'||str.charAt(i+1)=='C')) {
-                    j+=mp.get(str.charAt(i+1))-mp.get(str.charAt(i));
-                    i++;
-                } else if((i<str.length()-1&&str.charAt(i)=='C')&&(str.charAt(i+1)=='D'||str.charAt(i+1)=='M')) {
-                        j+=mp.get(str.charAt(i+1))-mp.get(str.charAt(i));
-                        i++;
-                    }else {
-                j+=mp.get(str.charAt(i));
-            }
+    public int romanToInt(String s) {
+       int ans = 0;
+int prev = 0;
 
-        }
-        return j;
+for (int i = s.length() - 1; i >= 0; i--) {
+
+    int num = 0;
+
+    switch (s.charAt(i)) {
+        case 'I': num = 1; break;
+        case 'V': num = 5; break;
+        case 'X': num = 10; break;
+        case 'L': num = 50; break;
+        case 'C': num = 100; break;
+        case 'D': num = 500; break;
+        case 'M': num = 1000; break;
+    }
+
+    if (num < prev) {
+        ans -= num;
+    } else {
+        ans += num;
+    }
+
+    prev = num;
+}
+
+return ans;
+        
     }
 }
