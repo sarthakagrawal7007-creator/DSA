41f06896d9ab94b09fdc06bf1949f4c491d20eaa
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-08-25 16:03:16 +0530

    Time: 382 ms (5.1%), Space: 47.5 MB (19.61%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,22 +1,22 @@
 class Solution {
     public String frequencySort(String s) {
-         HashMap<Character, Integer> map = new HashMap<>();
-
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+       int freq[]=new int[123];
+        for (char ch:s.toCharArray()){
+            freq[ch]++;
         }
-
-        // Convert map entries to list
-        List<Map.Entry<Character, Integer>> list =new ArrayList<>(map.entrySet());
-        System.out.println(list);
-
-        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
-
-        StringBuilder result = new StringBuilder();
-
-        for (Map.Entry<Character, Integer> entry : list) {
-            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        List<Character> l1=new ArrayList<>();
+        for (char ch:s.toCharArray()){
+            if (!l1.contains(ch)){
+                l1.add(ch);
+            }
         }
-         return result.toString();
+        String st="";
+        Collections.sort(l1,(a,b)->freq[b]-freq[a]);
+        for (char ch:l1){
+            for (int i = 0; i <freq[ch] ; i++) {
+                st+=ch;
+            }
+        }
+         return st;
     }
 }
