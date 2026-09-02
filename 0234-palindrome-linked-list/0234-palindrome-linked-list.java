33ca7656b52e3eb8fdb197127656b749ccac2d77
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-09-02 15:43:58 +0530

    Time: 3 ms (99.8%), Space: 94.8 MB (34.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -19,32 +19,33 @@ class Solution {
         temp=temp.next;
          size++;
       }
+      int size1=size/2;
           ListNode list1=head;
-          //ListNode lista=head;
-          for(int i=1;i<size/2;i++){
-            list1=list1.next;
+          ListNode lista=head;
+          for(int i=1;i<size1;i++){
+            lista=lista.next;
           }
           ListNode list2=null;
           if(size%2!=0){
-          list2=list1.next.next;
+          list2=lista.next.next;
           }else{
-          list2=list1.next;
+          list2=lista.next;
           }
-           list1.next=null;
+           lista.next=null;
            ListNode pre=null;
-           ListNode curr=head;
+           ListNode curr=list1;
            while(curr!=null){
             ListNode ne=curr.next;
             curr.next=pre;
             pre=curr;
             curr=ne;
            }
-           head=pre;
-           for(int i=1;i<=size/2;i++){
-            if(head.val!=list2.val){
+           list1=pre;
+           for(int i=1;i<=size1;i++){
+            if(list1.val!=list2.val){
                 return false;
             }
-            head=head.next;
+            list1=list1.next;
             list2=list2.next;
 
            }
