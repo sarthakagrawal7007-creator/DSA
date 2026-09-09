fa13abbde23af9079e062c1de80e3e452e5d1efd
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-09-09 17:17:08 +0530

    Time: 1 ms (100%), Space: 44 MB (9.78%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,36 +1,33 @@
 class Solution {
     public int myAtoi(String s) {
            int i = 0;
-        int sign = 1;
-        long sum = 0;
-         while (i < s.length() && s.charAt(i) == ' ') {
+           long sum = 0;
+        int l= s.length();
+         while (i < l && s.charAt(i) == ' ') {
             i++;
-        }
-
-        // 2. Check sign
-        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
-            i++;
-        }
-
-        // 3. Read digits
-        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-
-            sum = sum * 10 + (s.charAt(i) - '0');
-
-            // 4. Check overflow
-           int p=Integer.MAX_VALUE;
-           int q=Integer.MIN_VALUE;
-
-            if (sign*sum>=p){
-                 return p;}
-
-            else if (sign*sum<=q){
-                return q;}
-
+         }
+         int sig=1;
+          if (i < l && s.charAt(i) == '-') {
+            sig = -1;
+             i++;
+          } else if (i < l && s.charAt(i) == '+') {
            i++;
-        }
-        return (int)(sign*sum);
-
+         }
+           while(i<l &&s.charAt(i)=='0'){
+            i++;
+         }
+         while(i<l && (s.charAt(i)>='0'&&s.charAt(i)<='9')){
+            int r=(int)(s.charAt(i)-'0');
+            sum=sum*10+r;
+             if (sig==1&&sum>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }else if(sig==-1&&-sum<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            i++;
+         }
+         return (int)(sum*sig);
+         
     }
-}
+    }
+        
