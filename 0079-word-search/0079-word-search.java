37cae0421b147b9d6c823fe8dd5ae4ca20381b9d
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-09-22 13:10:15 +0530

    Time: 85 ms (96.28%), Space: 42.9 MB (75.44%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -16,34 +16,24 @@ class Solution {
         }
         return false;
     }
-    public static boolean b1(char[][] ch, String word,int i, int j, int s) {
-
-        if (s == word.length()){
+     public static boolean b1(char[][] ch, String word,int i, int j, int s) {
+        if (s==word.length()-1){
             return true;
         }
-
-        if (i < 0 || i >= ch.length || j < 0 || j >= ch[0].length) {
-            return false;
+         char temp=ch[i][j];
+         ch[i][j]='#';
+        if (i<ch.length-1 && ch[i+1][j]==word.charAt(s+1)){
+            if (b1(ch, word, i + 1, j, s + 1)) { ch[i][j] = temp; return true; }
+        }if (j>0&& ch[i][j-1]==word.charAt(s+1)){
+            if (b1(ch, word, i, j - 1, s + 1)) { ch[i][j] = temp; return true; }
         }
-
-        if (ch[i][j] != word.charAt(s)) {
-           return false;
+        if (j<ch[0].length-1&& ch[i][j+1]==word.charAt(s+1)){
+            if (b1(ch, word, i, j + 1, s + 1)) { ch[i][j] = temp; return true; }
         }
-        char temp = ch[i][j];
-        ch[i][j] = '$';
-
-        for (int[] dir : directions) {
-
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-
-            if (b1(ch, word, new_i, new_j, s+1)) {
-                return true;
-            }
+        if (i>0&& ch[i-1][j]==word.charAt(s+1)){
+            if (b1(ch, word, i - 1, j, s + 1)) { ch[i][j] = temp; return true; }
         }
-
-        ch[i][j] = temp;
-
+         ch[i][j]=temp;
         return false;
+    }
         }
-}
