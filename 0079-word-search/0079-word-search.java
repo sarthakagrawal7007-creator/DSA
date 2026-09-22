9760c0e23abf3c664724731146f7ea2e2dea0bf6
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-09-22 13:26:26 +0530

    Time: 115 ms (93.55%), Space: 42.8 MB (87.7%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,11 +1,4 @@
 class Solution {
-      static int m, n;
-    static int[][] directions = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1}
-    };
     public boolean exist(char[][] ch, String word) {
       for (int i = 0; i <ch.length; i++) {
             for (int j = 0; j <ch[0].length ; j++) {
@@ -26,18 +19,15 @@ class Solution {
 
          char temp=ch[i][j];
          ch[i][j]='#';
-       // if (i<ch.length-1 && ch[i+1][j]==word.charAt(s+1)){
+      
             if (b1(ch, word, i + 1, j, s + 1)) { ch[i][j] = temp; return true; }
-     //   }
-        //if (j>0&& ch[i][j-1]==word.charAt(s+1)){
+    
             if (b1(ch, word, i, j - 1, s + 1)) { ch[i][j] = temp; return true; }
-       // }
-     //   if (j<ch[0].length-1&& ch[i][j+1]==word.charAt(s+1)){
+     
             if (b1(ch, word, i, j + 1, s + 1)) { ch[i][j] = temp; return true; }
-       // }
-      //  if (i>0&& ch[i-1][j]==word.charAt(s+1)){
+       
             if (b1(ch, word, i - 1, j, s + 1)) { ch[i][j] = temp; return true; }
-       // }
+     
          ch[i][j]=temp;
         return false;
     }
