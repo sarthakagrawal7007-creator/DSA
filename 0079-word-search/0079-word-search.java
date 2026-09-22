9832ff02f454e5509227c02bf68022c16304f8ab
Sarthak Agrawal <sarthakagrawal7007@gmail.com>
2026-09-22 13:22:10 +0530

    Time: 116 ms (93.37%), Space: 42.7 MB (87.7%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -17,22 +17,27 @@ class Solution {
         return false;
     }
      public static boolean b1(char[][] ch, String word,int i, int j, int s) {
-        if (s==word.length()-1){
-            return true;
+         if (i>=ch.length||i<0 ||j>=ch[0].length ||j<0 ||ch[i][j]!=word.charAt(s) ){
+            return false;
         }
+         if (s==word.length()-1){
+            return true;
+         }
+
          char temp=ch[i][j];
          ch[i][j]='#';
-        if (i<ch.length-1 && ch[i+1][j]==word.charAt(s+1)){
+       // if (i<ch.length-1 && ch[i+1][j]==word.charAt(s+1)){
             if (b1(ch, word, i + 1, j, s + 1)) { ch[i][j] = temp; return true; }
-        }if (j>0&& ch[i][j-1]==word.charAt(s+1)){
+     //   }
+        //if (j>0&& ch[i][j-1]==word.charAt(s+1)){
             if (b1(ch, word, i, j - 1, s + 1)) { ch[i][j] = temp; return true; }
-        }
-        if (j<ch[0].length-1&& ch[i][j+1]==word.charAt(s+1)){
+       // }
+     //   if (j<ch[0].length-1&& ch[i][j+1]==word.charAt(s+1)){
             if (b1(ch, word, i, j + 1, s + 1)) { ch[i][j] = temp; return true; }
-        }
-        if (i>0&& ch[i-1][j]==word.charAt(s+1)){
+       // }
+      //  if (i>0&& ch[i-1][j]==word.charAt(s+1)){
             if (b1(ch, word, i - 1, j, s + 1)) { ch[i][j] = temp; return true; }
-        }
+       // }
          ch[i][j]=temp;
         return false;
     }
